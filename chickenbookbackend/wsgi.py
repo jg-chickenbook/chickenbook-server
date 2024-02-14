dc84be62eea3c0b89c48ca639ec545a4d63e07be
Aberrante <anaberrantis@gmail.com>
2024-02-14 23:25:51 +0100

    just project set up, with accounts app. djang

---
+++ chickenbookbackend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for chickenbookbackend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chickenbookbackend.settings')
+
+application = get_wsgi_application()
