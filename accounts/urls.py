489725a70a7460604293c3cb71bf4494f16e21cd
Jiří Trnečka <trnecka.jiri@gmail.com>
2024-03-09 19:50:06 +0100

    Corrected the urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('login', views.login, name="login"),
+    path('login', views.login, name="login"),
     path('register', views.register, name="register"),
     path('test_token', views.test_token, name="test_token"),
     path('logout', views.logout, name="logout"),
--- chickenbookbackend/urls.py
+++ chickenbookbackend/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-import accounts.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/login/', accounts.views.login, name='login'),
     path('api/accounts/', include('accounts.urls')),
 ]
